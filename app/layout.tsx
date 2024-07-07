806a456e5cbb742870d09c25961fd354ab73567b
hollali <dheztinykartel@gmail.com>
2024-07-07 12:45:16 +0000

    all

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,8 +1,15 @@
 import type { Metadata } from "next";
-import { Inter } from "next/font/google";
+import { Plus_Jakarta_Sans } from "next/font/google";
 import "./globals.css";
+import { cn } from '@/lib/utils'
+import { ThemeProvider } from "@/components/ui/theme-provider"
+import { RiFontSans } from "react-icons/ri";
 
-const inter = Inter({ subsets: ["latin"] });
+const inter = Plus_Jakarta_Sans({
+  subsets: ["latin"],
+  weight:['300', '400', '500', '600', '700',],
+  variable: '--font-sans'
+});
 
 export const metadata: Metadata = {
   title: "Cloak Care",
@@ -16,7 +23,17 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={cn('min-h-screen bg-dark-300 font-sans antialiased',)}>
+
+      <ThemeProvider
+            attribute="class"
+            defaultTheme="dark"
+            enableSystem
+            disableTransitionOnChange
+          >
+            {children}
+          </ThemeProvider>
+      </body>
     </html>
   );
 }
