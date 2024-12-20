053c01e8d1b59e483f5d63ccbc70adb22521caf2
hollali <dheztinykartel@gmail.com>
2024-12-20 11:42:56 +0000

    `feat: add patient creation functionality to patientForm and patient.actions`

--- lib/actions/patient.actions.ts
+++ lib/actions/patient.actions.ts
@@ -1,1 +1,15 @@
-export const createuser = async (user: CreateUserParams) => {}
+import { ID, Query } from "node-appwrite"
+import { users } from "../appwrite.config"
+
+export const createUser = async (user: CreateUserParams) => {
+    try {
+        const newUser = await users.create(ID.unique(), user.email, user.phone, undefined, user.name)
+    } catch(error : any){
+        if(error && error?.code===409) {
+            const documents = await users.list([
+                Query.equal('email', [user.email])
+            ])
+            return documents?.users[0]
+        }
+    }
+}
