cc3b9c427baf8784fba39424f3469cd73813a3a1
hollali <dheztinykartel@gmail.com>
2025-01-14 12:28:14 +0000

    `Update patient.actions.ts to include Appwrite config imports and server directive`

--- lib/actions/patient.actions.ts
+++ lib/actions/patient.actions.ts
@@ -1,23 +1,32 @@
-import { ID, Query } from "node-appwrite"
-import { users } from "../appwrite.config"
+"use server";
+import { ID, Query } from "node-appwrite";
 import { parseStringify } from "../utils";
+import {
+	BUCKET_ID,
+	DATABASE_ID,
+	ENDPOINT,
+	PATIENT_COLLECTION_ID,
+	PROJECT_ID,
+	databases,
+	storage,
+	users,
+} from "../appwrite.config";
 
+//! CREATE APPWRITE USER
 export const createUser = async (user: CreateUserParams) => {
-    try {
-        const newUser = await users.create(
-            ID.unique(), 
-            user.email, 
-            user.phone, 
-            undefined, 
-            user.name
-        );
-        return parseStringify(newUser)
-    } catch(error : any){
-        if(error && error?.code===409) {
-            const documents = await users.list([
-                Query.equal('email', [user.email])
-            ])
-            return documents?.users[0]
-        }
-    }
-}
+	try {
+		const newUser = await users.create(
+			ID.unique(),
+			user.email,
+			user.phone,
+			undefined,
+			user.name
+		);
+		return parseStringify(newUser);
+	} catch (error: any) {
+		if (error && error?.code === 409) {
+			const documents = await users.list([Query.equal("email", [user.email])]);
+			return documents?.users[0];
+		}
+	}
+};
